d9f0a698b212e7bba80aa1e0ad3f4b0e47f42897
richardjtelford <richard.telford@bio.uib.no>
2016-11-10 15:04:08 +0100

    file edited

--- Expectations/expectations.R
+++ Expectations/expectations.R
@@ -1,9 +1,13 @@
 library(readxl)
-expectations<-read_excel("expectations.xlsx")
-x11();par(mfrow=c(2,2))
-hist(expectations$Total, main="total")
-hist(expectations$Lead, main="lead")
-hist(expectations$Participated, main="participate")
+expectations <- read_excel("expectations.xlsx")
+x11()
+par(mfrow = c(2, 2))
+hist(expectations$Total, main = "total")
+hist(expectations$Lead, main = "lead")
+hist(expectations$Participated, main = "participate")
 plot(expectations$Participated, expectations$Lead)
-savePlot("Expectations.pdf", type="pdf")
-savePlot("Expectations.wmf", type="wmf")
+savePlot("Expectations.pdf", type = "pdf")
+savePlot("Expectations.wmf", type = "wmf")
+
+
+#file edited
