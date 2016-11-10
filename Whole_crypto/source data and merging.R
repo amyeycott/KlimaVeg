306b0ce0cdaf083a017b684a65ba7b4c919de9c9
amyeycott <amy@eycott.co.uk>
2016-11-10 14:59:29 +0100

    relative paths

--- Whole_crypto/source data and merging.R
+++ Whole_crypto/source data and merging.R
@@ -1,8 +1,8 @@
 #This project is to analyse all taxonomic groups in the crypto-klimaveg dataset. It was written by Amy Eycott starting 7th April 2016.
 #Before running any scripts in this project, check that the latest version of each database, and all supporting data sheets (e.g.ellenberg values) has been copied into the same project folder as this project, and that the folder is called "Whole crypto" with a capital W.
 
-source("\\\\helix.klient.uib.no\\biohome\\aey022\\rdata\\KlimaVeg\\Ania and Martin\\AniaMartinLichens_dataloading.R")
-source("\\\\helix.klient.uib.no\\biohome\\aey022\\rdata\\KlimaVeg\\sylwia\\Bryophyte data loading.R")#in an ideal world, work out which step is printing a bunch of structure...
+source("../Ania and Martin/AniaMartinLichens_dataloading.R")#the ../ is 'go up one level', doing it this way means that it finds the right directory whether it is on C, O, or another person's computer.
+source("../sylwia/Bryophyte data loading.R")#in an ideal world, work out which step is printing a bunch of structure...
 library(readxl)
 VascOld.thin<-as.data.frame(read_excel("KLIMAVEG_BIALOWIEZA_VASCULAR_OLD_Corr.xls"), col_types=c(rep("text",3), rep("numeric", 48), "text"))
 VascNew.thin<-as.data.frame(read_excel("KLIMAVEG_BIALOWIEZA_VASCULAR_2015.xls"))
