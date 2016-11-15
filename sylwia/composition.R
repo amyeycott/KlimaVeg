85e86d9f2dbc29af19c93331e1d5f528a378857a
amyeycott <amy@eycott.co.uk>
2016-11-15 16:55:48 +0100

    From some time ago, so I don't know what's different - committing to get them off the list.

--- sylwia/composition.R
+++ sylwia/composition.R
@@ -1,3 +1,4 @@
+source("Bryophyte data loading.R")
 library(vegan)
 coloury<-rep(c(1,2), nrow(easytabx))
 myfirstdca<-decorana(easytabx)
@@ -8,8 +9,9 @@ ordiarrows(myfirstdca, groups=substr(rownames(scores(myfirstdca)), 1,3), length=
 nmds1992<-metaMDS(easytabx[substr(rownames(easytabx),4,7)=="1992",])
 nmds2015<-metaMDS(easytabx[substr(rownames(easytabx),4,7)=="2015",])
 moss.crusty<-protest(nmds1992, nmds2015)
+moss.crusty#rho=0.98, p=0.001 :-)
 
-t.test(scores(myfirstdca[(substr(rownames(scores(myfirstdca)), 1,3))=="1992",1]),scores(myfirstdca[(substr(rownames(scores(myfirstdca)), 1,3))=="2015"]))
+t.test(scores(myfirstdca)[(substr(rownames(scores(myfirstdca)), 4,7))=="1992",1],scores(myfirstdca)[(substr(rownames(scores(myfirstdca)), 4,7))=="2015",1])# no difference between years, t283=0.561, p=0.5753
 
 plotswithviride2015<-bryophytes$Plot[bryophytes$Year==2015&bryophytes$Species_name=="Dicranum viride"]
 D_viride_subset<-bryophytes[bryophytes$Plot%in%plotswithviride2015,]
