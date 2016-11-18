a7ad402ecf51b014869a97e54c76c8ddebe65d82
amyeycott <amy@eycott.co.uk>
2016-11-18 17:08:29 +0100

    Just tidying

--- Whole_crypto/source data and merging.R
+++ Whole_crypto/source data and merging.R
@@ -37,7 +37,8 @@ VascNew.fat<-spread(VascNew.thin[,c(2,3,53)], Species_name_2015,frequency_score,
 rownames(VascNew.fat)<-VascNew.fat$Plot_number_2015
 VascNew.fat$Plot_number_2015<-NULL
 
-##making a combined file for vegdist: RT "Look at rioja::Merge or analogue::join"
+##making a combined file for vegdist
 library(analogue)
 Vascall.df<-join(VascOld.fat, VascNew.fat, na.replace=TRUE, split=FALSE, type="outer")
 
+#phytosociological communities
