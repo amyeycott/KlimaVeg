43ad2a10231128618daaf539ef0308077061c59e
amyeycott <amy@eycott.co.uk>
2017-01-04 18:59:14 +0100

    changed name of species indicator value/protection status to bryo.status to make whole crypto clearer

--- sylwia/Bryophyte data loading.R
+++ sylwia/Bryophyte data loading.R
@@ -2,15 +2,17 @@ library(readxl)
 bryophytes<-read_excel("../sylwia/Baza Styczen 2016.xls", sheet=1,col_names = TRUE) # przypisałam do zbioru funcje
 bryo.status<-read_excel("../sylwia/Baza Styczen 2016.xls", sheet="Ellenberg.protected")#works only if you index the sheet number by name, not by position.
 str(bryo.status)
-bryo.status$Red.coded[bryo.status$Red.coded==""]<-NA
-bryo.status$Any.status<-bryo.status$Red.coded
-bryo.status$Any.status[!is.na(bryo.status$Red.coded)]<-1
-bryo.status$Any.status[!is.na(bryo.status$Protected.species)]<-1
+names(bryo.status)<-gsub(" ", "_", names(bryo.status))
+bryo.status$Red_coded[bryo.status$Red_coded==""]<-0
+bryo.status$Any.status<-bryo.status$Red_coded
+bryo.status$Any.status[!is.na(bryo.status$Red_coded)]<-1
+bryo.status$Any.status[!is.na(bryo.status$Protected_species)]<-1
 bryo.status$Any.status[is.na(bryo.status$Any.status)]<-0
+bryo.status$Protected_species<-as.factor(bryo.status$Protected_species)
+bryo.status$Red_coded<-as.factor(bryo.status$Red_coded)
 
 
-
-str(bryophytes) # teraz robię badanie konstrukcji całyej zawartości arkusza, który został wczytany
+#str(bryophytes) # teraz robię badanie konstrukcji całyej zawartości arkusza, który został wczytany #commented out so that it's tidier when using this as source code
 bryophytes<-as.data.frame(bryophytes)# we do this because otherwise read.excel makes three objects and code won't work.
 bryophytes[is.na(bryophytes)] <-0#replace NAs with zeros. this makes some later code a lot easier but I always put dataset preparation at the top
 bryophytes<-bryophytes[!(rowSums(bryophytes[,5:length(bryophytes)]))==0,]#removes the blank rows. 
