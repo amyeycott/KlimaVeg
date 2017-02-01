5aba5f0259f679f75f365a3634125e2f37a7e0eb
richardjtelford <richard.telford@bio.uib.no>
2017-02-01 21:44:24 +0100

    tidying code, save objects

--- download_weather.R
+++ download_weather.R
@@ -1,33 +1,22 @@
-#Bialystok
+#Down load Bialowieza and regional temperature data 
 library("dplyr")
 library("rnoaa")
 library("ggplot2")
 library("tidyr")
 library("lubridate")
 
-#functions
-tidy_climate <- function(station, variable){
-  station %>% 
-    filter(element == variable) %>% 
-    select(id, year, month, element, matches("VALUE")) %>% 
-    gather(key = "day", value = "TAVG", -id, -year, -month, -element) %>%
-    mutate(TAVG = as.numeric(TAVG)) %>%
-    group_by(id, year, month, element) %>%
-    summarise(TAVG = mean(TAVG, na.rm = TRUE)/10, n = n()) %>%
-    mutate(date = as.Date(paste(year, month, 15, sep = "-")))
-}
 
 
-
+##regional climate
+#find nearby sites
 dat <- read.table(header = TRUE, sep = ",", text = "
   longitude, latitude, id
   23.894614, 52.744313,Białowieża"
 )
 
-stations <- meteo_nearby_stations(dat, lat_colname = "latitude",
-                      lon_colname = "longitude", station_data = ghcnd_stations(), var = "all",
-                      year_min = 1960, year_max = 2000, radius = NULL, limit = 20)
+stations <- meteo_nearby_stations(dat, lat_colname = "latitude", lon_colname = "longitude", station_data = ghcnd_stations(), var = "all",  year_min = 1960, year_max = 2000, radius = NULL, limit = 20)
 
+#map
 mp <- map_data("world", xlim = c(18, 30), ylim = c(48, 56))
 
 ggplot(stations$Białowieża, aes(x = longitude, y = latitude, label = name)) + 
@@ -36,33 +25,32 @@ ggplot(stations$Białowieża, aes(x = longitude, y = latitude, label = name)) +
   geom_label_repel() +
   geom_point(data = dat[1, ], aes(x = longitude, y = latitude), colour = "red", size = 3, inherit.aes = FALSE)
 
-
-Bialystok <- ghcnd(stationid = "PLM00012295")
-levels(Bialystok$element)
-
-
+#download data
 regionalData <- stations$Białowieża %>% 
-  filter(distance < 100) %>%
+  filter(distance < 100) %>% # 100 km radius
   group_by(id) %>%
   do(cbind(select(., name), ghcnd_search(.$id, var = "TAVG")$tavg)) %>% 
-  mutate(tavg = tavg/10)
+  mutate(tavg = tavg/10, 
+         variable = "tavg") %>%
+  rename(value = tavg)
 
-regionalData %>% filter(year(date) == 2000) %>% 
-  ggplot(aes(x = date, y = tavg, colour = name, group = name)) + geom_line()
+#plot regional data
+g <- regionalData %>% filter(year(date) == 2000) %>% 
+  ggplot(aes(x = date, y = value, colour = name, group = name)) + 
+    geom_line()
+print(g)
 
-regionalData %>%  
-  ggplot(aes(x = date, y = tavg, colour = name, group = name)) + geom_line()
+g %+% regionalData
 
 #monthly
-
 monthlyRegionalData <- regionalData %>% 
   mutate(month = month(date), year = year(date)) %>%
-  group_by(name, id, month, year) %>%
-  summarise(tavg = mean(tavg, na.rm = TRUE)) %>%
+  group_by(name, id, month, year, variable) %>%
+  summarise(value = mean(value, na.rm = TRUE)) %>%
   mutate(date = as.Date(paste(year, month, 15, sep = "-")))
 
 monthlyRegionalData %>% 
-  ggplot(aes(x = date, y = tavg, colour = name)) +
+  ggplot(aes(x = date, y = value, colour = name)) +
   geom_line() + 
   facet_wrap(~month, scale = "free_y")
 
@@ -83,30 +71,36 @@ meanTemperature <- plyr::ldply(seq(from = as.Date("1964-1-1"), to = as.Date("201
 
 save(meanTemperature, file = "phenology/data/BialowiezaMeanTemp.Rdata")
 
+BialowiezaDaily <- meanTemperature %>% 
+  rename(value = wartosc, date = data) %>% 
+  mutate(name = "Białowieża",
+         date = as.Date(format(date, "%Y-%m-%d")),
+         variable = "tavg")
 
 
-meanTemperature %>% ggplot(aes(x = data, y = wartosc))  + geom_line()
-meanTemperature %>% ggplot(aes(x = wartosc))  + geom_histogram()
+BialowiezaDaily %>% ggplot(aes(x = date, y = value, colour = variable))  + geom_line()
+BialowiezaDaily %>% ggplot(aes(x = value)) + geom_histogram()
 
 ## compare with regional data
-all_temperatures <- meanTemperature %>% 
-  rename(tavg = wartosc, date = data) %>% 
-  mutate(name = "Białowieża") %>%
-  mutate(date = as.Date(format(date, "%Y-%m-%d"))) %>%
-  bind_rows(regionalData)
+all_temperatures <- bind_rows(BialowiezaDaily, regionalData)
 
-filter(all_temperatures, year(date) == 1970) %>% 
-  ggplot(aes(x = date, y = tavg, colour = name)) +geom_line()
+g %+% filter(all_temperatures, year(date) == 1970) 
 
 #ALL MONTHLY
 monthly_all_temperatures <- all_temperatures %>% 
   filter(date >= "1964-01-01", date <= "2015-12-31") %>%
-  mutate(month = month(date), year = year(date)) %>%
-  group_by(name, id, month, year) %>%
-  summarise(tavg = mean(tavg, na.rm = TRUE)) %>%
-  mutate(date = as.Date(paste(year, month, 15, sep = "-")))
+  mutate(month = month.name[month(date)], 
+         month = factor(month, levels = month.name), 
+         year = year(date)) %>%
+  group_by(name, id, month, year, variable) %>%
+  summarise(value = mean(value, na.rm = TRUE)) %>%
+  mutate(date = as.Date(paste(year, month, 15, sep = "-"), format = "%Y-%B-%d"))
 
-monthly_all_temperatures  %>% 
-  ggplot(aes(x = date, y = tavg, colour = name)) +
+monthly_all_temperatures %>% 
+  ggplot(aes(x = date, y = value, colour = name)) +
   geom_line() + 
   facet_wrap(~month, scale = "free_y")
+
+
+##save data
+save(regionalData, BialowiezaDaily, monthly_all_temperatures, file = "phenology/data/downloaded_weather.Rdata")
