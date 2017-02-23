e07efe9bdb017cf592ef1a444cc804cb7277d3e4
richardjtelford <richard.telford@bio.uib.no>
2017-02-23 22:35:45 +0100

    moved function to own file

--- phenology/download_weather.R
+++ phenology/download_weather.R
@@ -56,22 +56,7 @@ monthlyRegionalData %>%
 
 
 ## Bialowieza data from 
-library("RCurl")
-library("readr")
-
-get_Polish_weather_data <- function(siteCode, variableCode, startDate, endDate, baseURL = "https://dane.imgw.pl/1.0/pomiary/cbdh/", authentic){
-  url0 <- paste0(baseURL, siteCode, "-", variableCode, "/tydzien/")
-  res <- plyr::ldply(seq(from = startDate, to = endDate, by = 7), function(D){#D is date
-    url <- paste0(url0, D,"?format=csv")
-    Sys.sleep(0.9)#don't hit server too hard
-    read_delim(getURL(url, userpwd = authentic), delim = ";")
-  }, .progress = "text")
-  
-  res %>%  
-    rename(value = wartosc, date = data) %>% 
-    filter(date >= startDate, date <= endDate) %>%
-    mutate(date = as.Date(format(date, "%Y-%m-%d")))
-}
+source("phenology/R/get_polish_weather.R")
 
 #authentic <- "richard.telford@uib.no:Pa55w0rd"#not real password
 #save(authentic, file = "phenology/data/authentic.Rdata")
